9c412357576a32b4dd6903343cf27eabaef277f4
Nadeem Akharware <nakharware@yahoo.co.uk>
2024-06-22 10:35:03 +0100

    Broken code. ToDo: Figure our why the JSON response won't parse

--- src/adhan.js
+++ src/adhan.js
@@ -8,14 +8,7 @@ window.onload = function() {
 }
 
 function initialise() {
-    const currentDate = new Date();
-
-    const currentTime = currentDate.toLocaleTimeString("en-EN", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false
-    });
-
+    getTimes();
     for(let row=0; row < salahNames.length; row++) {
         let salah = document.createElement("div");
         salah.id = salahNames[row];
@@ -30,6 +23,51 @@ function initialise() {
         document.getElementById("timetable").append(time);
     }
 
+    updateCurrentTime();
+    setInterval(updateCurrentTime, 1000);
+}
+
+function updateCurrentTime() {
+    const currentDate = new Date();
+
+    const currentTime = currentDate.toLocaleTimeString("en-EN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+    });
+
     let timeStamp = document.getElementById("currentTime");
     timeStamp.innerText = currentTime;
+}
+
+function getTimes() {
+    const apiUrl = 'https://api.aladhan.com/v1/calendarByCity/2024/6?city=London&country=United%20Kingdom&method=2';
+    let salahTime;
+    
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        salahTime = JSON.parse(data); // ToDo: Failing to parse the response, figure out why
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    if(typeof salahTime !== 'undefined') {
+        for(let day=0; day < salahTime.length; day++) {
+            let currentDate = salahTime[day].date.day;
+            if(currentDate = 22) {
+                let rowTime = document.getElementById("Fajr-time");
+                salahTimes[0] = salahTime[day].timings.Fajr;
+                rowTime.innerText = salahTimes[0];
+            }
+    
+        }
+    }    
 }
